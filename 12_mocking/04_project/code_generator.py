32f28a76069887fa62e054adc030eb47f80c43c4
wiejolap <wiejolap@gmail.com>
2022-06-29 16:18:45 +0200

    last chapter

---
+++ 12_mocking/04_project/code_generator.py
@@ -0,0 +1,17 @@
+import random
+from datetime import date
+
+
+def get_code():
+    rand_int = random.randint(0, 9)
+    return f'XC - {rand_int}'
+
+
+def get_today_name():
+    return date.today().strftime('%a')
+
+
+def get_code_with_day():
+    code = get_code()
+    dayname = get_today_name().upper()
+    return f'{code}-{dayname}'
